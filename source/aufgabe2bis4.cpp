ad995051f299e269673a5241de5643c2c22cb6e1
JaniTomati <jana.puschmann@uni-weimar.de>
2016-05-16 16:01:09 +0200

    Decide. Commit. Succeed. - irgendein Bild in  Google Bilder (finished 3.2 - 3.4)

--- source/aufgabe2bis4.cpp
+++ source/aufgabe2bis4.cpp
@@ -16,18 +16,15 @@ int main() {
   for (std::list<unsigned int>::iterator i = liste.begin(); i != liste.end(); ++i) {
     *i = std::rand() % 100 + 1; // Zufallszahlen von 0 - 100
   }
-  // Testausgabe
   // std::copy(std::begin(liste), std::end(liste), std::ostream_iterator<int>(std::cout, "\n"));
 
   // "Liste" wird in Vektor mit gleicher Groesse kopiert
   std::vector<unsigned int> vektor(liste.size());
   std::copy(std::begin(liste), std::end(liste), std::begin(vektor));
-  // Testausgabe
   // std::copy(std::begin(vektor), std::end(vektor), std::ostream_iterator<int>(std::cout, "\n"));
 
   // Erstellt ein Set aus Werten der Liste (Sortiert + ohne doppelte Werte)
   std::set<unsigned int> set_liste(std::begin(liste), std::end(liste)); // in range
-  // Testausgabe
   // std::copy(std::begin(set_liste), std::end(set_liste), std::ostream_iterator<int>(std::cout, "\n"));
   std::cout << "Die Liste enthaelt " << set_liste.size() << " verschiedene Werte." << std::endl;
   
@@ -40,4 +37,17 @@ int main() {
   }  
   std::cout << std::endl;
 
+  // Map: typdef pair<const key, T>, Elemente einer Liste sind immer sortiert nach dem "key"
+  // Erstellt Map und zaehlt die Haeufigkeit der Werte an Stelle i
+  std::map<unsigned int, unsigned int> map_liste;
+  for (std::list<unsigned int>::iterator i = liste.begin(); i != liste.end(); ++i) {
+    ++map_liste[*i];
+  }
+  // Ausgabe der Haeufigkeiten 
+  std::cout << "Wert : Häufigkeit" << std::endl;
+  for (int i = 0; i <= 100; ++i) {
+    std::cout << i << " : " << map_liste[i] << std::endl;
+  }
+
+  return 0;
 }
